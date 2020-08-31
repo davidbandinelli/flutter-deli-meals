d2d553baa018e8476849ae4a88089dd7408cb544
David Bandinelli <david.bandinelli@gmail.com>
2020-08-31 11:55:09 +0200

    filters and favorites

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -4,6 +4,11 @@ import '../dummy_data.dart';
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
 
+  final Function toggleFavorite;
+  final Function isFavorite;
+
+  MealDetailScreen(this.toggleFavorite, this.isFavorite);
+
   Widget buildSectionTitle(BuildContext context, String sectionTitle) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
@@ -73,6 +78,15 @@ class MealDetailScreen extends StatelessWidget {
                     ))),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(isFavorite(mealId) ? Icons.star : Icons.star_border),
+        onPressed: () => toggleFavorite(mealId),
+        /*
+        onPressed: () {
+          Navigator.of(context).pop(mealId);
+        },
+        */
       ),
     );
   }
